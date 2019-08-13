6a89346e245adb0d7f0152e64aede6a50a426bcc
Y.SEKIOKA <y.sekioka84@gmail.com>
2019-08-13 13:26:39 +0000

    PHP/Laravel 16 投稿したニュースを更新/削除しよう

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $table = 'profile';
     protected $guarded = array('id');
     
     public static $rules = array(
